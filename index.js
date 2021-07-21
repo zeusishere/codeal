cab32cdb61e08c73a4a4cf7f7a07ff90763f14f3
shubham singh <shubham1251997@gmail.com>
2021-07-21 20:35:26 +0530

    set up the MVC directory structure for our app

---
+++ index.js
@@ -0,0 +1,13 @@
+//we have installed express prior
+const express= require("express") ;
+const app = express();
+const port=8000;
+app.listen(port,function(err)
+{
+    if(err)
+    {
+        console.log(`error : ${err}`) ;
+        return ;
+    }
+    console.log(`server is running on ${port}`) ;
+} ) ;
